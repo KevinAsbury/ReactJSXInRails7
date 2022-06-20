856c94b086ca9d44f5a79be403da0d85c695fc72
Kevin Asbury <kevinasbury.dev@gmail.com>
2022-06-19 22:08:20 -0400

    Add input and console output

--- app/javascript/components/HelloWorld.jsx
+++ app/javascript/components/HelloWorld.jsx
@@ -1,5 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import _ from 'lodash';
 
-export default function HelloWorld() {
-  return <h1>Hello World from Reactjs!</h1>;
+export default function HelloWorld(props) {
+  [text, setText] = useState();
+
+  useEffect(() => {
+    if (!_.isEmpty(text)) {
+      console.log(`The text entered is: ${text}`);
+    }
+  }, [text]);
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    alert(`The text you entered was: ${text}`);
+    setText('');
+    console.clear();
+  };
+
+  return (
+    <div>
+      <h1>Hello World!</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Enter text:
+          <input
+            type="text"
+            name="name"
+            value={text}
+            onChange={e => setText(e.target.value)}
+          />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    </div>
+  );
 }
